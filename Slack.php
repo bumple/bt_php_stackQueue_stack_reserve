02baa2146f9edbb66f14e9c6b7810ff1b3debf31
thanhdeptrai <thanh.n5151@gmail.com>
2021-05-18 11:28:03 +0700

    first

---
+++ Slack.php
@@ -0,0 +1,31 @@
+<?php
+namespace ReverseArray;
+
+class Stack
+{
+    public array $stack;
+
+    public function __construct()
+    {
+        $this->stack = [];
+    }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->stack);
+    }
+
+    public function push($data)
+    {
+        array_push($this->stack,$data);
+    }
+
+    public function pop()
+    {
+        if(!$this->isEmpty()) {
+            array_pop($this->stack);
+        }else{
+            echo "Stack is Empty";
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+use ReverseArray\Stack;
+
+include_once "Stack.php";
+$stack = new Stack();
+
+$stack->push(5);
+$stack->push(4);
+$stack->push(3);
+$stack->push(2);
+$stack->push(1);
+
+while(!$stack->isEmpty())
+{
+    $stack->pop();
+}
+
+echo "<pre>";
+var_dump($stack);
